44d80ab995e40556c12316639791caeacf474c37
Mikael.Tadesse <Mikael.Tadesse@miningelement.com>
2025-06-18 14:34:51 +0300

    init project

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
 import pugPlugin from 'vite-plugin-pug'
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: 'index.pug'
+      input: {
+        main: resolve(__dirname, 'index.html') // Указываем HTML файл
+      }
     }
   },
-  plugins: [pugPlugin()]
+  plugins: [pugPlugin({
+    localImports: true
+  })]
 })
